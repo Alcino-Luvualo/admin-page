d0bee9c668d3a8d4b947e9925ea9af3869659557
alcino-luvualo <kenikenilson@gmail.com>
2025-08-06 03:26:06 +0100

    Criando Componente Painel

---
+++ src/Painel.jsx
@@ -0,0 +1,15 @@
+function Painel(){
+    return (
+        <div className='painel-container' >
+            <div className="header-painel">
+                <h2>Ola</h2>
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default Painel;
